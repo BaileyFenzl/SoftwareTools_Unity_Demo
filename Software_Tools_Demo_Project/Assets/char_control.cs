ee52a097ba0022df64612de1b54ce86986894953
Bailey Fenzl <bf743813@ohio.edu>
2016-11-05 16:04:21 -0400

    Added Unity Project, as well as the powerpoint

---
+++ Software_Tools_Demo_Project/Assets/char_control.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class char_control : MonoBehaviour {
+
+
+    // Use this for initialization. 
+    // This is called on the first frame
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    // These are all built-in functions 
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector2.left * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector2.right * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector2.up * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector2.down * Time.deltaTime);
+
+    }
+
+}
